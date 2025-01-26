a2119f363005d534dd29408a5d4840eaf9860466
Kennedy Emanoel <79938217+kennedyEmanoel@users.noreply.github.com>
2025-01-26 22:50:36 +0000

    add `database` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
 import database from "../../../../infra/database.js";
 
 async function status(request, response) {
-  const result = await datase.query("SELECT 1  + 1");
+  const result = await database.query("SELECT 1  + 1");
   console.log(result.rows);
   response.status(200).json({ chave: "são acima da média" });
 }
