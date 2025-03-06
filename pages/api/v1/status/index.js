8178d43432735a294f820e9538e183e489142a2a
Kennedy <kennedy1emanoel19@gmail.com>
2025-03-06 23:21:55 +0000

    compose.yaml

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,15 +1,24 @@
+/* import database from "../../../../infra/database";
+
 function status(request, response) {
-  response.status(200).json({ chave: "valor" });
+  console.log(database);
+  response.status(200).json({ chave: "são acima da média" });
 }
 
-export default status;
+export default status; */
 
-/* import database from "../../../../infra/database.js";
+import database from "../../../../infra/database.js";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1  + 1");
   console.log(result.rows);
   response.status(200).json({ chave: "são acima da média" });
+}
+
+export default status;
+
+/* function status(request, response) {
+  response.status(200).json({ chave: "valor" });
 }
 
 export default status; */
